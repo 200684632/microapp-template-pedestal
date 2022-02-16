319471db1021326fba73a4b98e4c895c63c3d25f
leipu <200684632@qq.com>
2022-02-16 17:40:12 +0800

    init

--- generator/index.js
+++ generator/index.js
@@ -16,7 +16,16 @@ module.exports = (api, _options = {}, rootOptions = {}) => {
 				"vue-router": "^3.5.2",
     		"vuex": "^3.6.2",
 				"element-ui": "^2.15.5",
-      }
+      },
+			"devDependencies": {
+				"@vue/cli-plugin-babel": "~4.5.0",
+				"@vue/cli-plugin-eslint": "~4.5.0",
+				"@vue/cli-service": "~4.5.0",
+				"babel-eslint": "^10.1.0",
+				"eslint": "^6.7.2",
+				"eslint-plugin-vue": "^6.2.2",
+				"vue-template-compiler": "^2.6.11"
+			},
     })
 
 		api.render('./template')
