6fa232b3600855085ba8df17b1329ecac7f56db1
leipu <200684632@qq.com>
2022-02-23 15:45:46 +0800

    调整

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports = (api) => {
-  api.configureWebpack(_ => {
-    return {
-      publicPath: '/',
-      devServer: {
-        port: 1779,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        }
-      },
+  api.configureWebpack(webpackConfig => {
+    webpackConfig.publicPath = '/'
+    webpackConfig.devServer = {
+      port: 1779,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
     }
   })
 }
