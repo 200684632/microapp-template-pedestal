f4da2c162a5b91aebbc512a61b9607776c0c1442
leipu <200684632@qq.com>
2022-02-23 11:31:23 +0800

    fixbug

--- generator/index.js
+++ generator/index.js
@@ -12,15 +12,15 @@ module.exports = (api, _options = {}, rootOptions = {}) => {
       "axios": "^0.21.1",
       "vue-router": "^3.5.2",
       "vuex": "^3.6.2",
-      "element-ui": "^2.15.5",
-      "core-js": "^3.6.5",
-      "GW-basics": "^0.1.0",
+      "core-js": "^3.6.5"
     },
     "devDependencies": {
       "vue-template-compiler": "^2.6.11",
+      "element-ui": "^2.15.5",
       "@vue/cli-plugin-babel": "~4.5.0",
       "@vue/cli-plugin-eslint": "~4.5.0",
       "@vue/cli-service": "~4.5.0",
+      "GW-basics": "^0.1.0",
       "babel-eslint": "^10.1.0",
       "node-sass": "^4.12.0",
       "sass-loader": "^7.0.1",
